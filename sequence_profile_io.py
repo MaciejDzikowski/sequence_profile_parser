88e9ca42f5c337e0d45a2744b7b31cd467760add
Maciej Dzikowski <mack.dzikowski@gmail.com>
2019-04-30 13:04:24 +0200

    spi fixes

--- sequence_profile_io.py
+++ sequence_profile_io.py
@@ -1,29 +1,31 @@
 import argparse
 import copy
 
+def pref_order():
+    """preferd order of of amino acids' symbols"""
+    order = ['VAL', 'ALA', 'PRO', 'ILE', 'LEU', 'MET', 'GLU', 'ASP', 'ARG',
+             'LYS', 'THR', 'SER', 'HIS', 'CYS', 'GLN', 'ASN', 'PHE', 'TYR',
+             'TRP', 'GLY', 'UNK', 'GAP', 'GPE']
+    return order
+
 def opening(file):
     """opening the file"""
     rows = []  # creating a list of rows
     for line in open(file):
         row = line.strip().split()
         rows.append(row)
-
     return rows
 
 def symbols(rows):
     """creating a list of amino acids' symbols"""
     names = rows[0][:]
-
     del names[0:2]  # deleting '#id' and 'aa'
-
     return names
 
 def aminoacids(rows):
     """creating a list of amino acids"""
     aa = [row[1] for row in rows]
-
     del aa[0]  # deleting 'aa'
-
     return aa
 
 def convert(m):
@@ -32,7 +34,6 @@ def convert(m):
     for row in range(len(m2)):
         for column in range(len(m2[row])):
             m2[row][column] = float(m2[row][column])
-
     return m2
 
 def r_matrix(rows):
@@ -40,20 +41,17 @@ def r_matrix(rows):
     r_mat = []
     for i in range(2, len(rows[0])):
         r_mat.append([rows[j][i] for j in range(1, len(rows))])
-
     return convert(r_mat)
 
 def match(input_sym, rmatrix):
     """matching the matrix to prefered order of amino acids' symbols"""
-    eg_symbols = ('ALA', 'VAL', 'PRO', 'ILE', 'LEU', 'MET', 'GLU', 'ASP',
-                  'ARG', 'LYS', 'THR', 'SER', 'HIS', 'CYS', 'GLN', 'ASN',
-                  'PHE', 'TYR', 'TRP', 'GLY', 'UNK', 'GAP', 'GPE')
+    eg_symbols = pref_order()
     sym = copy.deepcopy(input_sym)
-    r_m = copy.deepcopy(rmatrix)
 
     if sym == eg_symbols:
-        return r_m
+        return rmatrix
     else:
+        r_m = copy.deepcopy(rmatrix)
         new_matrix = []
         for symbol in sym:
             new_matrix.append(r_m[eg_symbols.index(symbol)])
@@ -64,14 +62,16 @@ def parse_sequence_profile(file):
     ([amino acids' symbols], [amino acids], [reversed matrix])
     """
     t_rows = opening(file)
+    t_pref_order = pref_order()
     t_symbols = symbols(t_rows)
     t_aminoacids = aminoacids(t_rows)
-    t_match = match(t_symbols, t_aminoacids)
-    tuple3 = (t_symbols, t_aminoacids, t_match)
+    t_rmatrix = r_matrix(t_rows)
+    t_match = match(t_symbols, t_rmatrix)
+    tuple3 = (t_pref_order, t_aminoacids, t_match)
     return tuple3
 
 if __name__ == "__main__" :
-    parser = argparse.ArgumentParser(description="Reads .profile file and prints\
+    parser = argparse.ArgumentParser(description="Reads .profile file, prints\
                                                   tuple with three elements:\
                                                   ([amino acids' symbols],\
                                                   [amino acids],\
